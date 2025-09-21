0f2223220bee3e4ba2c487545401df4da13b353c
Farhan <shahriarmahi.farhan@gmail.com>
2025-09-21 15:19:29 +0600

    ami change korlam

--- 2507011040_Q4.c
+++ 2507011040_Q4.c
@@ -7,6 +7,7 @@ int main()
     printf("The size of 'float' type variable (in bytes) : %zu\n", sizeof(float));
     printf("The size of 'char' type variable (in bytes)  : %zu\n", sizeof(char));
     printf("The size of 'double' type variable (in bytes): %zu", sizeof(double));
+    printf("hello my friend\n");
 
     return 0;
 }
